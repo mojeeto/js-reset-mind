5fae9b93a266c3766fc3840d3a889231077f72ee
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2025-12-18 15:50:17 +0330

    [basics] create the first function

--- basics/assets/scripts/app.js
+++ basics/assets/scripts/app.js
@@ -10,9 +10,24 @@
  * */
 const defaultResult = 0;
 let currentResult = defaultResult;
+// let name = "Mojtaba";
+
+// Create custom Function
+function sum(num1, num2) {
+  // result is local variable for fucntion
+  // but we can use the global variable from outside of function
+  // also we can use shadowing
+  result = num1 + num2;
+  // let name = "Hossein"; this is shadowing;
+  // alert("The result is " + result);
+  return result;
+}
+
+// Call the custom function
+currentResult = sum(1, 2);
 
 // do mathematical operaitors
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+//currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
