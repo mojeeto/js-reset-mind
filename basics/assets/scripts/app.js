971e7db435cf95c28601bc697e5fcaad477379a0
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2025-12-18 13:29:20 +0330

    [basics] create let variable

---
+++ basics/assets/scripts/app.js
@@ -0,0 +1,11 @@
+/*
+ * Variable names must be camelCase, it's can be start with:
+ *  $, _, Characters
+ * Valid Variable Names:
+ *  userName, numberGroup5, $kindOfOptional, _intervalValue
+ *
+ * InValid Variable Names:
+ *  Snake_Case, start with - (Dashes), or Whitespace in name
+ *  and don't use reserved keywords
+ * */
+const resultNumber = 0;
