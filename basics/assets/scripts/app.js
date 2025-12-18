79000e079f1c1b866ba4358556b29772a4eb4aa6
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2025-12-18 15:57:10 +0330

    [basics] use addEventListener for plus button in html

--- basics/assets/scripts/app.js
+++ basics/assets/scripts/app.js
@@ -13,7 +13,7 @@ let currentResult = defaultResult;
 // let name = "Mojtaba";
 
 // Create custom Function
-function sum(num1, num2) {
+/*function sum(num1, num2) {
   // result is local variable for fucntion
   // but we can use the global variable from outside of function
   // also we can use shadowing
@@ -21,14 +21,21 @@ function sum(num1, num2) {
   // let name = "Hossein"; this is shadowing;
   // alert("The result is " + result);
   return result;
-}
+}*/
 
 // Call the custom function
-currentResult = sum(1, 2);
+//currentResult = sum(1, 2);
 
 // do mathematical operaitors
 //currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+//let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
-outputResult(currentResult, calculationDescription);
+function add() {
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, "");
+}
+
+addBtn.addEventListener("click", add);
+
+//outputResult(currentResult, calculationDescription);
