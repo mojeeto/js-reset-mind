c9d91f5059c08c609a597678bd7055231fd34688
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-02 15:54:26 +0330

    [feat] add logic to backdrop and add movie button
    when you click [Add Movie] button in header, the modal will show.
    after clicking the black screen behind of modal that called backdrop the
    modal will display none.
    also the cancel button in add movie modal have logic to close the modal
    and backdrop.

---
+++ movie-project/assets/scripts/app.js
@@ -0,0 +1,32 @@
+// connect to add movie button on header
+const addMovieButton = document.querySelector("header button");
+// connect to hidden modal for adding new movie
+const addMovieModal = document.getElementById("add-modal");
+// connet to cancel button in addMovieModal
+const cancelAddMovieModal =
+  addMovieModal.querySelector(".modal__actions").firstElementChild;
+// connect to the backdrop
+const backdrop = document.getElementById("backdrop");
+
+// function for visible backdrop and addMovieModal
+const toggleAddMovieModal = () => {
+  // toggle the hidden modal for adding new movie
+  // also show the backdrop
+  addMovieModal.classList.toggle("visible");
+  backdrop.classList.toggle("visible");
+};
+
+// Add event listener for clicking addMovieButton
+addMovieButton.addEventListener("click", () => {
+  toggleAddMovieModal();
+});
+
+// add event listner for clicking cancel button of addMovieModal
+cancelAddMovieModal.addEventListener("click", () => {
+  toggleAddMovieModal();
+});
+
+// Add event listner for clicking backdrop
+backdrop.addEventListener("click", () => {
+  toggleAddMovieModal();
+});
