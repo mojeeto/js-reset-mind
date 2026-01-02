4c4dbffc9559ceefb5b2f8e0279acc46fc03c97a
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-02 16:21:12 +0330

    [feat] validation userinput for adding new movie detail

--- movie-project/assets/scripts/app.js
+++ movie-project/assets/scripts/app.js
@@ -2,9 +2,13 @@
 const addMovieButton = document.querySelector("header button");
 // connect to hidden modal for adding new movie
 const addMovieModal = document.getElementById("add-modal");
+// connect to all inputs in addMovieModal
+const userInputs = addMovieModal.querySelectorAll("input");
 // connet to cancel button in addMovieModal
 const cancelAddMovieModal =
   addMovieModal.querySelector(".modal__actions").firstElementChild;
+// connect to add button in addMovieModal
+const acceptAddMovieModal = cancelAddMovieModal.nextElementSibling;
 // connect to the backdrop
 const backdrop = document.getElementById("backdrop");
 
@@ -16,8 +20,33 @@ const toggleAddMovieModal = () => {
   backdrop.classList.toggle("visible");
 };
 
+// function for handling userinputs data for adding
+// new movie detail
+const addNewMovieHandler = () => {
+  const title = userInputs[0].value;
+  const imageUrl = userInputs[1].value;
+  const rating = userInputs[2].value;
+
+  // validation
+  if (
+    title.trim() === "" ||
+    imageUrl.trim() === "" ||
+    rating.trim() === "" ||
+    +rating < 1 ||
+    +rating > 5
+  ) {
+    alert("Invalid input data. (rating must be between 1 and 5).");
+    return;
+  }
+
+  console.log("OK");
+};
+
 // Add event listener for clicking addMovieButton
 addMovieButton.addEventListener("click", toggleAddMovieModal);
+
+// add event listener for clicking add button in addMovieModal
+acceptAddMovieModal.addEventListener("click", addNewMovieHandler);
 
 // add event listner for clicking cancel button of addMovieModal
 cancelAddMovieModal.addEventListener("click", toggleAddMovieModal);
