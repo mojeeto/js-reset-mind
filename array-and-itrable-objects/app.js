c1842583c90e0986bb08e1b6e11eb01b45c31719
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-03 17:27:38 +0330

    [array] create array with Array constructor function

--- array-and-itrable-objects/app.js
+++ array-and-itrable-objects/app.js
@@ -1,3 +1,10 @@
 // the best way to create an array
 const numbers = [1, 2, 3];
-console.log("Best practice to create an array:", numbers);
+
+// constructor function for create new array
+// new array(5) ==> create array with 5 length
+// new array(1,2) ==> create array with 2 length [1,2]
+// new array("Hi", "Mojtaba") ==> create array with 2 length ['Hi', 'Mojtaba']
+const moreNumbers = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+// Also we can user Array instead of new array
+const moreNumbers2 = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
