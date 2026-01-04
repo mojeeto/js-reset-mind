f4b87dde21f1694500f5bce62fa5591f59e76933
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-04 10:20:42 +0330

    [object] delete property and assign new one

--- objects/assets/scripts/objects.js
+++ objects/assets/scripts/objects.js
@@ -8,4 +8,16 @@ const person = {
   },
 };
 
+// deleting properties
+delete person.age;
+// we can use null or undefined but the best way is delete option
+// undefined that menas for programmers in javascript the age property
+// not exists means if we assing the age to undefined programmers will know that
+// we don't use age property again
+// but if we assign it to null then that means programmers must know that age
+// is exists and we use it again but for somehow we don't assign a data to it
+// assign a new property
+person.isAdming = true;
+
 person.greet();
+console.log(person);
