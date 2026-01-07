42c7c20e4707ecfad58d85db62ad6f604a41eaa1
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-07 16:12:24 +0330

    [constructor-function] working with prototype property in constructor function

--- constructor-functions/assets/scripts/main.js
+++ constructor-functions/assets/scripts/main.js
@@ -26,7 +26,46 @@ function Person() {
 
 // it's create an object from function because of new keyword
 const person = new Person();
-person.greet();
+
+console.log(
+  "This is a Person Constructor Function & Full information about below that",
+);
 console.log(Person);
-console.log(typeof person);
-console.log(person instanceof Person);
+console.dir(Person);
+
+console.log("This is a Person Constructor Function Object & Full information");
+console.log(person);
+
+console.log(`
+Let add some function to base class
+the prototype property in constructor function it self can be like extends from some class in classes in javascript
+Person(class).prototype(property, that added by default) = {
+  in this object we can add some properties or methods that behavior like
+  extends from other constructor function or class
+}
+`);
+
+// as we learn the this keyword refer to which object that created base on class call the method
+Person.prototype = {
+  customProperty: "Something",
+  tellMeYourAge() {
+    console.log(
+      `The custom property: ${this.customProperty}\nThe Age: ${this.age}`,
+    );
+  },
+};
+
+const person2 = new Person();
+
+console.log(
+  "This is a Person Constructor Function & Full information about below that After Customization the prototype property in Person",
+);
+console.log(Person);
+console.dir(Person);
+
+console.log(
+  "This is a New Person Constructor Function Object & Full information",
+);
+console.log(person2);
+
+person2.tellMeYourAge();
