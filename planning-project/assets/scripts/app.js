fad55ee6784c2bccab874c3ea553c38c133ff98b
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-07 23:55:55 +0330

    [feat] store ProjectItems

--- planning-project/assets/scripts/app.js
+++ planning-project/assets/scripts/app.js
@@ -1,14 +1,25 @@
+class ProjectItem {
+  constructor(id) {
+    this.id = id;
+  }
+}
+
 class ProjectList {
+  #projects = [];
+
   constructor(type) {
     const projects = document.querySelectorAll(`#${type}-projects li`);
-    console.log(projects);
+    for (const project of projects) {
+      this.#projects.push(new ProjectItem(project.id));
+    }
+    console.log(this.#projects);
   }
 }
 
 class App {
   static init() {
-    const activeProjects = new ProjectList("active");
-    const finishedProjects = new ProjectList("finished");
+    new ProjectList("active");
+    new ProjectList("finished");
   }
 }
 
