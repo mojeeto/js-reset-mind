fc4f364402444d82dcfa58530953ca7b438420bc
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-08 17:30:07 +0330

    [learning] getBoundingClientRect

---
+++ adv-dom-sizes-html/assets/scripts/sizes.js
@@ -0,0 +1,7 @@
+const mainBox = document.getElementById("main-box");
+
+// this method show use the width height and position of pixel in the browser window
+console.log(mainBox.getBoundingClientRect());
+console.log(
+  "We have to know the browser render our page from top to bottom not bottom to top",
+);
