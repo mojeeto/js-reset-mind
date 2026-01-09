eb693beb7a56cb74fa2378caba1f1df4288f29c0
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-09 21:18:06 +0330

    [event] add event from js file

--- events/assets/scripts/events.js
+++ events/assets/scripts/events.js
@@ -1,1 +1,12 @@
 const button = document.querySelector("button");
+
+// Other ways to add event to button
+button.onclick = () => {
+  console.log("Hello, World!");
+};
+
+function someFunction() {
+  console.log("Some Function Event");
+}
+
+button.onclick = someFunction;
