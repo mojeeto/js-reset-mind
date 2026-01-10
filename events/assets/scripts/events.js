98ea81d21948f98badab0aa80bae84529a81f51c
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-10 11:54:50 +0330

    [event] underestanding bubbling and capturing phase

--- events/assets/scripts/events.js
+++ events/assets/scripts/events.js
@@ -1,4 +1,5 @@
 const button = document.querySelector("button");
+const div = document.querySelector("div");
 
 // Other ways to add event to button
 /*
@@ -8,18 +9,27 @@ button.onclick = () => {
 */
 
 function someFunction(event) {
+  /* 
   event.target.disabled = true;
   event.target.style.backgroundColor = "gray";
+  */
   console.log("Some Function Event");
 }
 
 // button.onclick = someFunction;
 
+// after click on button the div event listener also runs
+// because of bubbling feature in js
 button.addEventListener("click", someFunction);
+div.addEventListener("click", () => {
+  console.log("Hello, Im Dive");
+});
 
+/* 
 setTimeout(() => {
   button.removeEventListener("click", someFunction);
 }, 2000);
+*/
 
 const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
