715d178377f96a2dfd44f26f888ca3741a1f167f
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-10 10:57:10 +0330

    [event] removeEventListener

--- events/assets/scripts/events.js
+++ events/assets/scripts/events.js
@@ -1,12 +1,20 @@
 const button = document.querySelector("button");
 
 // Other ways to add event to button
+/*
 button.onclick = () => {
   console.log("Hello, World!");
 };
+*/
 
 function someFunction() {
   console.log("Some Function Event");
 }
 
-button.onclick = someFunction;
+// button.onclick = someFunction;
+
+button.addEventListener("click", someFunction);
+
+setTimeout(() => {
+  button.removeEventListener("click", someFunction);
+}, 2000);
