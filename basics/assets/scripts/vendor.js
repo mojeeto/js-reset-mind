53a5b8cef7202263bbf89451cffb84955ad3d6bb
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-12 12:24:16 +0330

    [numbers] more about numbers

--- basics/assets/scripts/vendor.js
+++ basics/assets/scripts/vendor.js
@@ -1,13 +1,19 @@
-const userInput = document.getElementById('input-number');
-const addBtn = document.getElementById('btn-add');
-const subtractBtn = document.getElementById('btn-subtract');
-const multiplyBtn = document.getElementById('btn-multiply');
-const divideBtn = document.getElementById('btn-divide');
+const userInput = document.getElementById("input-number");
+const addBtn = document.getElementById("btn-add");
+const subtractBtn = document.getElementById("btn-subtract");
+const multiplyBtn = document.getElementById("btn-multiply");
+const divideBtn = document.getElementById("btn-divide");
 
-const currentResultOutput = document.getElementById('current-result');
-const currentCalculationOutput = document.getElementById('current-calculation');
+const currentResultOutput = document.getElementById("current-result");
+const currentCalculationOutput = document.getElementById("current-calculation");
 
 function outputResult(result, text) {
   currentResultOutput.textContent = result;
   currentCalculationOutput.textContent = text;
 }
+
+// all numbers in javascript is float
+console.log("Max Safe Integer(2^53)-1:", Number.MAX_SAFE_INTEGER);
+console.log("Max Min Safe Integer(2^53)-1:", Number.MIN_SAFE_INTEGER);
+console.log("Max Max Value:", Number.MAX_VALUE);
+console.log("Max Min Value:", Number.MIN_VALUE);
