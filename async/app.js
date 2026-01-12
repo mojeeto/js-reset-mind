33efb1c80e626fe04ee78a0207cd8a8f130dbce5
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-12 18:22:42 +0330

    [async] start learning

---
+++ async/app.js
@@ -0,0 +1,9 @@
+const button = document.querySelector("button");
+const output = document.querySelector("p");
+
+function trackUserHandler() {
+  console.log("Clicked!");
+}
+
+button.addEventListener("click", trackUserHandler);
+
