fe42dee593109161f1abb265d4182eaa1b82b107
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-13 11:00:45 +0330

    [async] js not block the code

--- async/app.js
+++ async/app.js
@@ -2,11 +2,21 @@ const button = document.querySelector("button");
 const output = document.querySelector("p");
 
 function trackUserHandler() {
-  console.log("Clicked!");
+  navigator.geolocation.getCurrentPosition(
+    (data) => {
+      console.log(data);
+    },
+    (error) => {
+      console.log(error);
+    },
+  );
+  // this line execute first because the js not block the code for getting permission from user
+  console.log("Getting user position...");
 }
 
 button.addEventListener("click", trackUserHandler);
 
+/*
 let result = 0;
 
 for (let i = 0; i < 1000000000; i++) {
@@ -14,4 +24,5 @@ for (let i = 0; i < 1000000000; i++) {
 }
 
 console.log(result);
+*/
 
