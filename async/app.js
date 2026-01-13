eea3ddb13211973994800dd7daf1e00a0e70d7cc
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-13 11:46:15 +0330

    [promise] chaning promise feature

--- async/app.js
+++ async/app.js
@@ -1,25 +1,40 @@
 const button = document.querySelector("button");
 const output = document.querySelector("p");
 
+const getPosition = (opt) =>
+  new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (data) => resolve(data),
+      (error) => reject(error),
+      opt,
+    );
+  });
+
 const setTimer = (duration) =>
   new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("DONE!");
-    }, duration);
+    try {
+      setTimeout(() => {
+        resolve("DONE!");
+      }, duration);
+    } catch (err) {
+      reject(err);
+    }
   });
 
 function trackUserHandler() {
-  navigator.geolocation.getCurrentPosition(
-    (data) => {
-      console.log(data);
-    },
-    (error) => {
-      setTimer(2000).then((data) => {
-        console.log(data);
-      });
-      // console.log(error);
-    },
-  );
+  // in iran i dont have internet yet
+  let data;
+  getPosition()
+    .then((posData) => {
+      console.log(posData);
+    })
+    .catch((err) => {
+      data = err;
+      return setTimer(2000);
+    })
+    .then((d) => {
+      console.log(d, data);
+    });
   // this line execute first because the js not block the code for getting permission from user
   console.log("Getting user position...");
 }
