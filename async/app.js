1ab44992d898cfecd64727b069fb86ad93817f7e
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-13 11:36:22 +0330

    [promise] promisable browser setTimout API(v1)

--- async/app.js
+++ async/app.js
@@ -1,13 +1,23 @@
 const button = document.querySelector("button");
 const output = document.querySelector("p");
 
+const setTimer = (duration) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("DONE!");
+    }, duration);
+  });
+
 function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(
     (data) => {
       console.log(data);
     },
     (error) => {
-      console.log(error);
+      setTimer(2000).then((data) => {
+        console.log(data);
+      });
+      // console.log(error);
     },
   );
   // this line execute first because the js not block the code for getting permission from user
