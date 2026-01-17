73b7a10b6f548dd197c3b4293387321d7b9bdc33
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-17 14:03:49 +0330

    [learning] other solution for fib alg

--- algorithms/mathAlgorithms/fib.js
+++ algorithms/mathAlgorithms/fib.js
@@ -13,8 +13,15 @@ function fib(index) {
   return result;
     */
   // recursion way
+  /*
   if ([0, 1].includes(index)) return 1;
   return fib(index - 1) + fib(index - 2);
+  */
+  const numbers = [1, 1];
+  for (let i = 2; i <= index; i++)
+    numbers.push(numbers[i - 2] + numbers[i - 1]);
+  // console.log(numbers);
+  return numbers[index];
 }
 
 console.log(fib(4)); // should return 5
