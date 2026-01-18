6ce75a6e9d2a512bc52c87f808dead742064cb2d
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-18 12:57:02 +0330

    [learning] without recursive

--- algorithms/mathAlgorithms/factorial.js
+++ algorithms/mathAlgorithms/factorial.js
@@ -1,6 +1,12 @@
 function fact(number) {
-  if (number === 1) return 1;
-  return number * fact(number - 1);
+  // recursive
+  // if (number === 1) return 1;
+  // return number * fact(number - 1);
+  // without recursive
+  let result = number;
+  for (let i = 1; i < number; i++) result *= i;
+  return result;
+  // O(n)
 }
 
 console.log(fact(3)); // 6
