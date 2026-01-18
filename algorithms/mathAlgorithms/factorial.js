9e1ee983afad3e9a7a2b0bc6a8def9958ce04737
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-18 13:08:31 +0330

    [learning] time complexity for function recursive

--- algorithms/mathAlgorithms/factorial.js
+++ algorithms/mathAlgorithms/factorial.js
@@ -1,11 +1,16 @@
 function fact(number) {
   // recursive
-  // if (number === 1) return 1;
-  // return number * fact(number - 1);
+  if (number === 1) return 1;
+  return number * fact(number - 1);
+  // In every function call => O(1)
+  // But we trigger multiple function calls => n functions call
+  // T = n * O(1) => O(n)
   // without recursive
+  /*
   let result = number;
   for (let i = 1; i < number; i++) result *= i;
   return result;
+  */
   // O(n)
 }
 
