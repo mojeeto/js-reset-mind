5d3a98379e71d280b2ee4cddf7056f09795eb73a
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-19 15:28:29 +0330

    [learning] bubble sorting alg

---
+++ algorithms/sortAlgorithms/bubble-sort.js
@@ -0,0 +1,16 @@
+function sort(unSortedArray) {
+  const sortedArray = [...unSortedArray]; // copy the array not change the reference array
+  for (let outer = 0; outer < sortedArray.length; outer++)
+    for (let inner = outer + 1; inner < sortedArray.length; inner++)
+      if (sortedArray[outer] > sortedArray[inner]) {
+        const outerValue = sortedArray[outer];
+        sortedArray[outer] = sortedArray[inner];
+        sortedArray[inner] = outerValue;
+      }
+  return sortedArray;
+}
+
+const unSortedList = [5, 10, -3, -10, 1, 100, 99];
+const sortedList = sort(unSortedList);
+console.log(unSortedList);
+console.log(sortedList);
