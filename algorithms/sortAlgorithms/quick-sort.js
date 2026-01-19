919fe3929faf38d46e6b55ee88774942ea9a46bb
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-19 16:54:52 +0330

    [learning] quick sort alg

---
+++ algorithms/sortAlgorithms/quick-sort.js
@@ -0,0 +1,22 @@
+function sort(unSortedArray) {
+  const copiedArray = [...unSortedArray];
+  if (copiedArray.length <= 1) return copiedArray;
+  const pvotElement = copiedArray.shift();
+  const centerElements = [pvotElement];
+  const smallerElements = [];
+  const biggerElements = [];
+  while (copiedArray.length) {
+    const currentElement = copiedArray.shift();
+    if (currentElement === pvotElement) centerElements.push(currentElement);
+    else if (currentElement > pvotElement) biggerElements.push(currentElement);
+    else smallerElements.push(currentElement);
+  }
+  const smallestElementsArray = sort(smallerElements);
+  const biggestElementsArray = sort(biggerElements);
+  return smallestElementsArray.concat(centerElements, biggestElementsArray);
+}
+
+const unSortedArray = [-3, 10, 1, 100, -3, -10, 22, 15];
+const sortedArray = sort(unSortedArray);
+console.log("Un-Sorted Array", unSortedArray);
+console.log("Sorted Array", sortedArray);
