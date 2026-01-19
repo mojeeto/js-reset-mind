1e13c7de6e055c80f2770f1761082feee7c86677
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-19 11:58:03 +0330

    [learnig] binary search alg

---
+++ algorithms/searchAlgorithms/binary-search.js
@@ -0,0 +1,16 @@
+function findElement(sortedArray, element) {
+  let startIndex = 0;
+  let endIndex = sortedArray.length - 1;
+  while (startIndex <= endIndex) {
+    console.log("STEP>>>");
+    const middleIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+    if (sortedArray[middleIndex] === element) return middleIndex;
+    if (sortedArray[middleIndex] < element) startIndex = middleIndex + 1;
+    else endIndex = middleIndex - 1;
+  }
+  return -1;
+}
+
+const arr = [1, 5, 9, 13, 99, 100];
+
+console.log(findElement(arr, 9));
