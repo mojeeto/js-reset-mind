5af7de71d75f99e66c81d7c4cf99599e4fd7ac1f
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-20 16:34:42 +0330

    [learning] more sets

--- algorithms/setsArrayAlgorithms/app.js
+++ algorithms/setsArrayAlgorithms/app.js
@@ -3,9 +3,10 @@
 function cartProduct(setA, setB) {
   if (setA.length < 1 || setB.length < 1) return [];
   const sets = [];
-  for (const itemA of setA) {
+  for (let itemA of setA) {
+    if (!Array.isArray(itemA)) itemA = [itemA];
     for (const itemB of setB) {
-      sets.push([itemA, itemB]);
+      sets.push([...itemA, itemB]);
     }
   }
   return sets;
@@ -15,6 +16,17 @@ function cartProduct(setA, setB) {
 // create new array per each array
 // Space Complexity: O(n*m)
 
-const colors = ["blue", "red"];
-const sizes = ["m", "l"];
+function cartesian(...sets) {
+  let temp = sets[0];
+  for (const items of sets.slice(1)) temp = cartProduct(temp, items);
+  return temp;
+}
+
+// Time Complexity: O(n^x)
+// Space Complexity: O(n^x)
+
+const colors = ["blue", "red", "green", "black", "purple"];
+const sizes = ["s", "m", "l", "x", "xl"];
+const styles = ["round neck", "v neck"];
 console.log(cartProduct(colors, sizes)); // [[blue, m], [blue, l], ...]
+console.log(cartesian(colors, sizes, styles));
