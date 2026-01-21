1e55ab95160fc855621e557b6165c2768e164bfd
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-21 22:31:40 +0330

    [learning] recursive way of knapsack

--- algorithms/knapsackProblem.js
+++ algorithms/knapsackProblem.js
@@ -1,37 +1,31 @@
-function knapsack(items, maxCapacity) {
-  let result = [];
-  let value = 0;
-  let capacity = 0;
-  for (const item1 of items) {
-    for (const item2 of items) {
-      if (item1 === item2) continue;
-      if (item1.weight <= maxCapacity) {
-        if (item1.value > value) {
-          value = item1.value;
-          capacity = item1.weight;
-          result.push(item1);
-        }
-      }
+function knapsack(items, maxCapacity, itemIndex) {
+  if (maxCapacity === 0 || itemIndex < 0)
+    return {
+      items: [],
+      value: 0,
+      weight: 0,
+    };
 
-      if (item2.weight <= maxCapacity) {
-        if (item2.value > value) {
-          value = item2.value;
-          capacity = item2.weight;
-          result.pop();
-          result.push(item2);
-        }
-      }
+  if (items[itemIndex].weight > maxCapacity)
+    return knapsack(items, maxCapacity, itemIndex - 1);
 
-      if (item1.weight + item2.weight <= maxCapacity) {
-        if (item1.value + item2.value > value) {
-          value = item1.value + item2.value;
-          capacity = item1.weight + item2.weight;
-          result = [item1, item2];
-        }
-      }
-    }
-  }
-  return result;
+  const sackWithItem = knapsack(
+    items,
+    maxCapacity - items[itemIndex].weight,
+    itemIndex - 1,
+  );
+  const sackWithoutItem = knapsack(items, maxCapacity, itemIndex - 1);
+
+  const valueSackWithItem = sackWithItem.value + items[itemIndex].value;
+  const valueSackWithoutItem = sackWithoutItem.value;
+
+  if (valueSackWithItem > valueSackWithoutItem)
+    return {
+      items: sackWithItem.items.concat(items[itemIndex]),
+      value: valueSackWithItem,
+      weight: sackWithItem.weight + items[itemIndex].weight,
+    };
+  else sackWithoutItem;
 }
 
 const items = [
@@ -51,6 +45,6 @@ const items = [
     weight: 3,
   },
 ];
-const maxCapacity = 14;
+const maxCapacity = 8;
 
-console.log(knapsack(items, maxCapacity));
+console.log(knapsack(items, maxCapacity, items.length - 1));
