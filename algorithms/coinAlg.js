5447e2f900af8d5a8d123b83a6dcb09ea3a3971a
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-22 16:26:31 +0330

    [learning] other solution for coins alg

--- algorithms/coinAlg.js
+++ algorithms/coinAlg.js
@@ -1,4 +1,5 @@
 function computeChange(coins, amount) {
+  /*
   let counter = 0;
   const result = {};
   for (const coin of coins) {
@@ -11,10 +12,27 @@ function computeChange(coins, amount) {
       }
   }
   return result;
+    */
+  // other solution
+  let remaningAmount = amount;
+
+  const result = {
+    selectedCoins: {},
+    numberOfCoins: 0,
+  };
+
+  for (const coin of coins) {
+    const counter = Math.floor(remaningAmount / coin);
+    result.numberOfCoins += counter;
+    result.selectedCoins[coin] = counter;
+    remaningAmount -= coin * counter;
+  }
+
+  return result;
 }
 
 const availableCoins = [100, 50, 20, 10, 5, 2, 1];
-const targetAmount = 130;
+const targetAmount = 129;
 
 const change = computeChange(availableCoins, targetAmount);
-console.log(change);
+console.log("Target Amount is:", targetAmount, "\n", change);
