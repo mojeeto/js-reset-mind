46849d368f75ef89ce6a76a2d4cb40c900f78acd
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-24 17:31:38 +0330

    [learning] fetch post by click fetchPost button and use promise feature

--- working-with-http/assets/scripts/app.js
+++ working-with-http/assets/scripts/app.js
@@ -1,24 +1,36 @@
+const url = "https://jsonplaceholder.typicode.com/";
 const postItemTemplate = document.getElementById("post-item-template");
 const postList = document.querySelector(".posts");
-// create XMLHttpRequest Instance
-const xhrInstance = new XMLHttpRequest();
-// configure the instance of xhr
-let method = "GET";
-const url = "https://jsonplaceholder.typicode.com/";
-let path = "/posts";
-xhrInstance.open(method, `${url}${path}`);
-// this line make the response JSON.parse(xhrInstance.response) as default
-// so we don't need to write it again
-xhrInstance.responseType = "json";
-// add listener for when xhr response
-xhrInstance.onload = () => {
-  const { response: items } = xhrInstance;
+const fetchPostButton =
+  document.getElementById("available-posts").firstElementChild;
+
+function sendHttpRequest(method, path) {
+  const url = "https://jsonplaceholder.typicode.com" + path;
+  // create XMLHttpRequest Instance
+  const xhrInstance = new XMLHttpRequest();
+  // configure the instance of xhr
+  xhrInstance.open(method, url);
+  // this line make the response JSON.parse(xhrInstance.response) as default
+  // so we don't need to write it again
+  xhrInstance.responseType = "json";
+  return new Promise((resolve, reject) => {
+    // add listener for when xhr response
+    xhrInstance.onload = () => {
+      resolve(xhrInstance.response);
+    };
+    // send the request
+    xhrInstance.send();
+  });
+}
+
+async function getPosts() {
+  const items = await sendHttpRequest("GET", "/posts");
   for (const item of items) {
     const postItemNode = document.importNode(postItemTemplate.content, true);
     postItemNode.querySelector("h2").textContent = item.title.toUpperCase();
     postItemNode.querySelector("p").textContent = item.body;
     postList.appendChild(postItemNode);
   }
-};
-// send the request
-xhrInstance.send();
+}
+
+fetchPostButton.addEventListener("click", getPosts);
