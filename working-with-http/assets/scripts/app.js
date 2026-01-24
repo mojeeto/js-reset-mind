46e6236884748bf335ea5e7643d5e03877ded459
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-24 16:49:03 +0330

    [learning] using xhr feature for parse body to json and use it as list item

--- working-with-http/assets/scripts/app.js
+++ working-with-http/assets/scripts/app.js
@@ -1,3 +1,5 @@
+const postItemTemplate = document.getElementById("post-item-template");
+const postList = document.querySelector(".posts");
 // create XMLHttpRequest Instance
 const xhrInstance = new XMLHttpRequest();
 // configure the instance of xhr
@@ -5,5 +7,18 @@ let method = "GET";
 const url = "https://jsonplaceholder.typicode.com/";
 let path = "/posts";
 xhrInstance.open(method, `${url}${path}`);
+// this line make the response JSON.parse(xhrInstance.response) as default
+// so we don't need to write it again
+xhrInstance.responseType = "json";
+// add listener for when xhr response
+xhrInstance.onload = () => {
+  const { response: items } = xhrInstance;
+  for (const item of items) {
+    const postItemNode = document.importNode(postItemTemplate.content, true);
+    postItemNode.querySelector("h2").textContent = item.title.toUpperCase();
+    postItemNode.querySelector("p").textContent = item.body;
+    postList.appendChild(postItemNode);
+  }
+};
 // send the request
 xhrInstance.send();
