77a1d5420b0d2f170e57fa29b9a4c5d112889667
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-30 16:26:29 +0330

    [learning] getting uid from localStorage

--- storage/app.js
+++ storage/app.js
@@ -1,3 +1,14 @@
+const storeBtn = document.getElementById('store-btn');
+const retrBtn = document.getElementById('retrieve-btn');
+
 const userId = 'userId_1';
 
-localStorage.setItem("uid", userId);
+storeBtn.addEventListener('click', () => {
+  localStorage.setItem("uid", userId);
+})
+
+retrBtn.addEventListener('click', () => {
+  const uid = localStorage.getItem("uid");
+  if (uid) console.log('the user id is ' + uid);
+  else console.log("uid not found!");
+})
