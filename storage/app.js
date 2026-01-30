b6ea44cb5afb492e6f26da441bcc63297cd24739
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2026-01-30 16:34:32 +0330

    [learning] sessionStorage and store the simple object with JSON in localstorage

--- storage/app.js
+++ storage/app.js
@@ -2,13 +2,26 @@ const storeBtn = document.getElementById('store-btn');
 const retrBtn = document.getElementById('retrieve-btn');
 
 const userId = 'userId_1';
+const userData = {
+  name: 'Mojtaba',
+  age: 24,
+  hobbies: ['driving', 'programming'],
+}
 
+// SessionStorage is live until the tab or the web application not closed
+// and if we close the browser the sessionstorage will erase
+// but localStorage is live until the user or browser delete it manualy
+
+// JSON data is string data
 storeBtn.addEventListener('click', () => {
-  localStorage.setItem("uid", userId);
+  sessionStorage.setItem("uid", userId);
+  localStorage.setItem('userData', JSON.stringify(userData));
 })
 
 retrBtn.addEventListener('click', () => {
-  const uid = localStorage.getItem("uid");
+  const uid = sessionStorage.getItem("uid");
+  const user = localStorage.getItem("userData");
+  console.log(JSON.parse(user))
   if (uid) console.log('the user id is ' + uid);
   else console.log("uid not found!");
 })
